f2cd894c7e2f841f4a35b5eadb8b319b3d40bcc6
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-03-13 19:16:34 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+//Built from work by Ahmed Haque in github.com/afhaque/MeanMapAppV2.0
+
+//Dependencies
+var express         = require('express');
+var mongoose        = require('mongoose');
+var port            = process.env.PORT || 3000;
+var morgan          = require('morgan');
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var app             = express();
+
+//Express Configuration
+
+//Set connection to MongoDB
+mongoose.connect("mongodb://localhost/MeanMapApp");
+
+//Logging and Parsing
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+//Routes
+// require('./app/routes.js')(app);
+
+//Listen
+app.listen(port);
+console.log('App listening on port ' + port);
